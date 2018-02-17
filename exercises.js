c696130369bd18ef87c16b3a96b154324c882ac3
Sean Kagawa <seankagawa@gmail.com>
2018-02-16 16:39:52 -1000

    compl ex11

--- exercises.js
+++ exercises.js
@@ -320,6 +320,16 @@ console.log(plainBoxResult);
     Invoke your function and pass in your stockCar object, store the result to a variable named isAutomaticTransmission and use `console.log` to inspect your results.
  */
 
+function detectingTransmission(obj){
+  if(obj.automaticTransmission === true){
+    return "The car has an automatic transmission.";
+  }else{
+    return "The car has a manual transmission.";
+  }
+}
+
+var isAutomaticTransmission = detectingTransmission(stockCar);
+console.log(isAutomaticTransmission);
 
 /*
 12.  Who's driving this thing?!
